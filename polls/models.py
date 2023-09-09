c3701c88c4f26694af9c6a0e02ed98740e36fbc5
Thorung Boonkaew <thorung.b@ku.th>
2023-09-09 14:08:06 +0700

    completed Question Class

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,8 @@ from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    end_date = models.DateTimeField('ending date for voting', null=True, blank=True)
 
     def __str__(self):
         return self.question_text
@@ -20,7 +21,19 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    def is_published(self):
+        now = timezone.now()
+        return self.pub_date <= now
 
+    def can_vote(self):
+        now = timezone.now()
+        if not self.is_published():
+            return False
+        if self.end_date is None:
+            return True
+        else:
+            return now <= self.end_date
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
