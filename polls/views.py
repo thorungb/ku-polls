526289e611df17c064e6be17583cca527737b443
thorungb <thorungboonmail@gmail.com>
2023-09-10 15:07:44 +0700

    Add correctly-formatted docstring comments to the models.py and views.py

--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,9 @@ from django.utils import timezone
 
 
 class IndexView(generic.ListView):
+    """
+    View for displaying the list of the latest published questions.
+    """
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
@@ -16,9 +19,15 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
+    """
+    View for displaying the details of a specific question.
+    """
     model = Question
     template_name = 'polls/detail.html'
 
@@ -27,18 +36,28 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
-    
+
 
 class ResultsView(generic.DetailView):
+    """
+    View for displaying the results of a specific question.
+    """
     model = Question
     template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
+    """
+    View for handling user votes on a question.
+    Returns A redirect to the results page or
+    a rendering of the detail page with an error message.
+    """
     question = get_object_or_404(Question, pk=question_id)
     if question.can_vote():
         try:
-            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+            selected_choice = question.choice_set.get(
+                pk=request.POST['choice']
+            )
         except (KeyError, Choice.DoesNotExist):
             # Redisplay the question voting form.
             return render(request, 'polls/detail.html', {
@@ -51,9 +70,11 @@ def vote(request, question_id):
             # Always return an HttpResponseRedirect after successfully dealing
             # with POST data. This prevents data from being posted twice if a
             # user hits the Back button.
-            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+            return HttpResponseRedirect(
+                reverse('polls:results', args=(question.id,))
+            )
     else:
-        return render(request,'polls/detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "This poll cannot be voted on at this time!!!"
-        })         
+        })
