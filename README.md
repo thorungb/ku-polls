b7a4d25b46a1cfe1174ee15127c6a69ec2138860
thorungb <thorungboonmail@gmail.com>
2023-09-16 08:54:46 +0700

    edit blank line

--- README.md
+++ README.md
@@ -27,4 +27,5 @@ All project-related documents are in the [Project Wiki](https://github.com/thoru
 - [Development Plan](https://github.com/thorungb/ku-polls/wiki/Development-Plan)
 - [Iteration 1 Plan](https://github.com/thorungb/ku-polls/wiki/Iteration-1-Plan)
 - [Iteration 2 Plan](https://github.com/thorungb/ku-polls/wiki/Iteration-2-Plan)
+- [Iteration 3 Plan](https://github.com/thorungb/ku-polls/wiki/Iteration-3-Plan)
 - [Task Board](https://github.com/users/thorungb/projects/6)
--- polls/views.py
+++ polls/views.py
@@ -24,7 +24,7 @@ class IndexView(generic.ListView):
         ).order_by('-pub_date')[:5]
 
 
-class DetailView(generic.DetailView):
+class DetailView(LoginRequiredMixin, generic.DetailView):
     """
     View for displaying the details of a specific question.
     """
@@ -53,28 +53,27 @@ def vote(request, question_id):
     a rendering of the detail page with an error message.
     """
     question = get_object_or_404(Question, pk=question_id)
-    if question.can_vote():
-        try:
-            selected_choice = question.choice_set.get(
-                pk=request.POST['choice']
-            )
-        except (KeyError, Choice.DoesNotExist):
-            # Redisplay the question voting form.
-            return render(request, 'polls/detail.html', {
-                'question': question,
-                'error_message': "You didn't select a choice.",
-            })
-        else:
-            selected_choice.votes += 1
-            selected_choice.save()
-            # Always return an HttpResponseRedirect after successfully dealing
-            # with POST data. This prevents data from being posted twice if a
-            # user hits the Back button.
-            return HttpResponseRedirect(
-                reverse('polls:results', args=(question.id,))
-            )
-    else:
+    if not question.can_vote():
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "This poll cannot be voted on at this time!!!"
         })
+    try:
+        selected_choice = question.choice_set.get(
+        pk=request.POST['choice']
+        )
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(
+        reverse('polls:results', args=(question.id,))
+        )
